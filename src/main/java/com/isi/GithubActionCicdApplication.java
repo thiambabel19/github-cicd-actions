1d1aa2a474c9dd37e1f5cf89862e79420e0e8914
thiambabel19 <mohamedthiam563@gmail.com>
2024-02-17 17:36:08 +0000

    first-commit

---
+++ src/main/java/com/isi/GithubActionCicdApplication.java
@@ -0,0 +1,13 @@
+package com.isi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionCicdApplication.class, args);
+	}
+
+}
